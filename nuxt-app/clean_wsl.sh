2d8d13d3462693e437143ae7e5d5d7d10acacbfd
LEBaz2211 <92997786+LEBaz2211@users.noreply.github.com>
2024-10-01 14:52:23 +0200

    fix deployment.yaml

--- nuxt-app/clean_wsl.sh
+++ nuxt-app/clean_wsl.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
-
-set -e
+#!/bin/bash -e
 
 echo "Starting cleanup process for nuxt-app..."
 
 # Step 1: Delete Kubernetes deployments and services
 echo "Deleting Kubernetes deployments and services..."
-kubectl delete -f ./deployment.yaml
-kubectl delete -f ./service.yaml
-kubectl delete -f ./ingress.yaml
+minikube kubectl -- delete -f ./deployment.yaml
+minikube kubectl -- delete -f ./service.yaml
+minikube kubectl -- delete -f ./ingress.yaml
 
 echo "Cleanup process for nuxt-app completed successfully."
